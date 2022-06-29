cee2ecdc30e87ae68fac3ab03d3bff45b6f02fff
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-06-29 22:25:21 +0200

    chore: added domains

--- src/helpers/utils/random-uuid.h
+++ src/helpers/utils/random-uuid.h
@@ -3,10 +3,17 @@
 using namespace std;
 
 string generate_uuid() {
-    // generate uniqui uuid 16 bytes
-    string uuid = "";
-    for (int i = 0; i < 16; i++) {
-        uuid += to_string(rand() % 10);
-    }
-    return uuid;
+       UUID uuid;
+    RPC_CSTR  uuid_str;
+    std::string uuid_out;
+
+    if (UuidCreate(&uuid) != RPC_S_OK)
+        std::cout << "couldn't create uuid\nError code" << GetLastError() << std::endl;
+
+    if (UuidToStringA(&uuid, &uuid_str) != RPC_S_OK)
+        std::cout << "couldn't convert uuid to string\nError code" << GetLastError() << std::endl;
+
+    uuid_out = (char*)uuid_str;
+    RpcStringFreeA(&uuid_str);
+    return uuid_out;
 }
